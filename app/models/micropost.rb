089af04eec73427463db6db266985c8e6fc7f24b
monsteronfire <monsteronfire@gmail.com>
2015-10-25 14:02:36 +0800

    adds validation got content and user_id

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,8 @@
 class Micropost < ActiveRecord::Base
   validates :content, length: { maximum: 140 }
 
+  validates :content, presence: true
+  validates :user_id, presence: true
+
   belongs_to :user
 end
